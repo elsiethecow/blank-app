0da5c7a923764e6deb39cccba82203a3cf45a559
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-09-23 21:28:21 +0000

    wowowowoo

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,4 @@ st.audio("minions.mp3",autoplay=True)
 st.image("minionguitar.png")
 st.image("minionsgun.jpg")
 
+st.feedback(options="thumbs", *, key=None, disabled=False, on_change=None, args=None, kwargs=None)
