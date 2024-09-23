aea2dcd9ec73cf126f8289cc6ecf36a1d24a3aa9
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-09-23 19:16:22 +0000

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,4 @@ st.title("Mininons!")
 st.audio("minions.mp3",autoplay=True)
 st.image("https://i2.pngimg.me/thumb/f/350/comdlpng6966452.jpg")
 st.image("https://i1.sndcdn.com/artworks-000127653259-xj2m6d-t500x500.jpg")
+
