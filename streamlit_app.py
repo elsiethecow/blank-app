d8c7a021ec47ff70bb19e89f0c90c9f451b33cf8
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-09-23 19:10:56 +0000

    minions

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.title("It's not rice")
-st.video(http://www.facebook.com)
+st.title("Mininons!")
+st.audio("minions.mp3",autoplay=True)
+st.image("https://i2.pngimg.me/thumb/f/350/comdlpng6966452.jpg")
+st.image("https://i1.sndcdn.com/artworks-000127653259-xj2m6d-t500x500.jpg")
