c8a0b6fdbf2652264d141683bbb3ead7dd2b83f9
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-09-23 19:37:16 +0000

    minions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Mininons!")
 st.audio("minions.mp3",autoplay=True)
-st.image("https://i2.pngimg.me/thumb/f/350/comdlpng6966452.jpg")
-st.image("https://i1.sndcdn.com/artworks-000127653259-xj2m6d-t500x500.jpg")
+st.image("minionguitar.png")
+st.image("minionsgun.jpg")
 
