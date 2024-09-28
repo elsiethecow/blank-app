5d2cbbb54d082c015e8be6d0aa0d9a40320b6276
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-09-28 21:12:45 +0000

    wowowowo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,31 @@
+import datetime
 import streamlit as st
 
-st.title("Mininons!")
-st.audio("minions.mp3",autoplay=True)
-st.image("minionguitar.png")
-st.image("minionsgun.jpg")
+st.title("About you.")
 
-st.feedback(options="thumbs", *, key=None, disabled=False, on_change=None, args=None, kwargs=None)
+wow = st.text_input("full name", "Name")
+st.write("Hello", wow)
+
+age = st.slider("How old are you?", 5, 20, 5)
+st.write("your ", age, "years old")
+
+genre = st.radio(
+    "What's your computer experiencs",
+    [":rainbow[A LOT]", "***avrige***", " very little"],
+    index=None,
+)
+
+st.write("You selected:", genre)
+
+color = st.color_picker("Pick A Color", "#00f900")
+st.write("The current color is", color)
+
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
+
+st.write("your rating of computers")
+
+sentiment_mapping = [":material/thumb_down:", ":material/thumb_up:"]
+selected = st.feedback("thumbs")
+if selected is not None:
+    st.markdown(f"You selected: {sentiment_mapping[selected]}")
