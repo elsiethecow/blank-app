a0f79aca6d4c0ca363967c2f3dcb40fd3c5c14c0
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-10-04 18:25:31 +0000

    wow

--- pages/couculater.py
+++ pages/couculater.py
@@ -1,40 +1,23 @@
 import streamlit as st
 
-if st.button("Say hello"):
-    st.write("Why hello there")
-
-num1 = st.number_input("this number", value=4)
-
-
-num2 = st.number_input("and this number", value=5)
-
-st.write(" =", num2+num1)
-
-num1 = st.number_input("this number ", value=4)
-
-
-num2 = st.number_input("times this number", value=5)
-
 st.write("=", num2*num1)
 
 
-num1 = st.number_input("this number  ", value=4)
+num1 = st.number_input("Number1  ", value=4)
 
 
-num2 = st.number_input("mines this number ", value=5)
+num2 = st.number_input("Number2 ", value=5)
 
-st.write(" =", num1-num2)
-
-
-if st.button("finished"):   
-    st.write("Goodbye!")
+if st.button("Devide"):
+    st.write("num1 % num2=", num2%num1)
 
 
+if st.button("Times"):
+    st.write("num1 * num2=", num2*num1)
 
 
+if st.button("minuse"):
+    st.write("num1 - num2=", num2-num1)
 
-
-
-
-
-
+if st.button("plus")
+    st.write("num1 + num2=", num2+num1)
