ad8bdbd2be53fddad9c3dbd6edb7b9d76efe9403
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-11-18 19:35:38 +0000

    igiugiutiuy

--- pages/cats.py
+++ pages/cats.py
@@ -2,7 +2,7 @@
 import streamlit as st
 import requests
 
-response = requests.get("https://pokeapi.co/api/v2/pokemon/16/")
+response = requests.get("https://pokeapi.co/api/v2/pokemon/17/")
 
 st.slider("find you favorit pokemon", 1, 1025, 1)
 st.image(response.json()["sprites"]["front_default"])
