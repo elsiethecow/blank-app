0f52bd62eb3c73be0f0d54def9fb27a5612a30f0
elsiethecow <96492769+elsiethecow@users.noreply.github.com>
2024-11-18 19:29:44 +0000

    hubabuh

--- pages/cats.py
+++ pages/cats.py
@@ -5,4 +5,4 @@ import requests
 response = requests.get("https://pokeapi.co/api/v2/pokemon/1025/")
 
 st.slider("find you favorit pokemon", 1, 1025, 1)
-st.link_button(response.json()["sprites"]["front_default"])
+st.image_button(response.json()["sprites"]["front_default"])
